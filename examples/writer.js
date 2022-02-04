e4651c58adb810f51b0974fa607ab7fdf4661ce5
Amelia Thompson <amelia@comparative.ai>
2022-02-04 11:16:20 -0500

    Added example code for writer

--- examples/writer.js
+++ examples/writer.js
@@ -4,4 +4,15 @@
 
 const lib = require('../lib')
 
-const reader = new lib.ParquetWriter('example-out.parquet')
+const schema = {
+  field_0: { type: 'INT64' },
+  field_1: { type: 'UTF8' },
+}
+
+const writer = new lib.ParquetWriter(schema, 'example-out.parquet')
+writer.open()
+writer.appendRow({
+  field_0: 1,
+  field_1: 'Hello World!',
+})
+writer.close()
